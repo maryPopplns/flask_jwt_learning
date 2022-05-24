e752b186116c592e474570d02a75e1ca77db15f6
maryPopplns <spencer.knightusn@gmail.com>
2022-05-24 10:50:24 -0700

    uri update

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ load_dotenv()
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+uri = os.getenv('DATABASE_URL')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
